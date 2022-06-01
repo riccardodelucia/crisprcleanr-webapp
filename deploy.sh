8ed0723d2fa0c3569353f3ffe67df8339731e216
riccardo.delucia <riccardo.delucia@fht.org>
2022-06-01 16:56:30 +0200

    fix on deploy folder

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source /home/riccardo.delucia/docker-compose/bin/activate
-cd /home/riccardo.delucia/deploy/iorio-apps
+cd /home/riccardo.delucia/deploy/crisprcleanr_webapp
 docker-compose pull
 docker-compose down
-docker-compose -p iorio_apps up --no-build -d
+docker-compose -p crisprcleanr_webapp up --no-build -d
