55ab6b8837ddc473dd3fea66c71b3d285ca74c6a
riccardo.delucia <riccardo.delucia@fht.org>
2022-06-01 17:17:12 +0200

    fix on docker-compose down remove-orphans

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 source /home/riccardo.delucia/docker-compose/bin/activate
 cd /home/riccardo.delucia/deploy/crisprcleanr_webapp
 docker-compose pull
-docker-compose down
+docker-compose down --remove-orphans
 docker-compose -p crisprcleanr_webapp up --no-build -d
